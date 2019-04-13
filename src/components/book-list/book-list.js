451f1be7aa22c0ef278d9dffda3dcea5aa2ea717
ducatimonster821 <s.asusk56c@gmail.com>
2019-04-13 19:35:00 +0500

    lesson131

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -15,23 +15,7 @@ import './book-list.css';
 class BookList extends Component {
 
   componentDidMount() {
-    const {
-      bookstoreService,
-      booksLoaded,
-      booksRequested,
-      booksError } = this.props;
-
-    booksRequested();
-
-    // bookstoreService.getBooks()
-    //   .then((data) => {
-    //     console.log('data:', data);
-    //     booksLoaded(data);
-    //   });
-    
-    bookstoreService.getBooks()
-      .then((data) => booksLoaded(data))
-      .catch((err) => booksError(err));
+    this.props.fetchBooks();
   }
 
   render() {
@@ -69,10 +53,24 @@ const mapStateToProps = ({books, loading, error}) => {
   }
 };
 
-const mapDispatchToProps = {
-  booksLoaded,
-  booksRequested,
-  booksError
+// const mapDispatchToProps = {
+//   booksLoaded,
+//   booksRequested,
+//   booksError
+// };
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const { bookstoreService } = ownProps;
+  return {
+    fetchBooks: () => {
+      console.log('Fetching Books');
+
+      dispatch(booksRequested());
+      bookstoreService.getBooks()
+        .then((data) => dispatch(booksLoaded(data)))
+        .catch((err) => dispatch(booksError(err)));
+    }
+  }
 };
 
 export default compose(
