3bb4b24de3ac52a6a21eb073549d52378d5892b4
ducatimonster821 <s.asusk56c@gmail.com>
2019-04-13 13:07:58 +0500

    lesson126

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import BookListItem from '../book-list-item';
-import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+
+import {withBookstoreService} from '../hoc';
+import {booksLoaded} from "../../actions";
+import {compose} from '../../utils';
 
 import './book-list.css';
 
 class BookList extends Component {
+
+  componentDidMount() {
+    // 1. receive data
+    const {bookstoreService} = this.props;
+    const data = bookstoreService.getBooks();
+    console.log(data);
+
+    // 2. dispatch action to store
+    this.props.booksLoaded(data);
+  }
+
   render() {
     const {books} = this.props;
     return (
@@ -22,11 +38,22 @@ class BookList extends Component {
 }
 
 // const mapStateToProps = (state) => {
-const mapStateToProps = ({ books }) => {
+const mapStateToProps = ({books}) => {
   return {
     // books: state.books
     books
   }
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = {
+  booksLoaded
+};
+
+export default compose(
+  withBookstoreService(),
+  connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
+
+// export default withBookstoreService()(
+//   connect(mapStateToProps, mapDispatchToProps)(BookList)
+// );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,8 @@
 const initialState = {
-  books: [
-    {
-      id: 1,
-      title: 'Production-Ready Microservices',
-      author: 'Susan J. Fowler'
-    },
-    {
-      id: 2,
-      title: 'Relese It!',
-      author: 'Michael T. Nygard'
-    },
-  ]
+  books: []
 };
 
 const reducer = (state = initialState, action) => {
-
   switch (action.type) {
     case 'BOOKS_LOADED':
       return {
