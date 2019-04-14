7575b5b80e38e3fa2d2d6a88b2d4ceeaf0e27f0c
ducatimonster821 <s.asusk56c@gmail.com>
2019-04-14 19:52:54 +0500

    lesson135

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -3,7 +3,7 @@ import BookListItem from '../book-list-item';
 import {connect} from 'react-redux';
 
 import {withBookstoreService} from '../hoc';
-import {fetchBooks} from '../../actions';
+import {fetchBooks, bookAddedToCart} from '../../actions';
 import {compose} from '../../utils';
 
 import Spinner from '../spinner';
@@ -11,13 +11,17 @@ import ErrorIndicator from '../error-indicator';
 
 import './book-list.css';
 
-const BookList = ({books}) => {
+const BookList = ({books, onAddedToCart}) => {
   return (
     <ul className="book-list">
       {
         books.map((book) => {
           return (
-            <li key={book.id}><BookListItem book={book} /></li>
+            <li key={book.id}>
+              <BookListItem
+                book={book}
+                onAddedToCart={() => onAddedToCart(book.id)} />
+            </li>
           )
         })
       }
@@ -32,7 +36,7 @@ class BookListContainer extends Component {
   }
 
   render() {
-    const {books, loading, error} = this.props;
+    const {books, loading, error, onAddedToCart} = this.props;
 
     if (loading) {
       return <Spinner />;
@@ -42,7 +46,7 @@ class BookListContainer extends Component {
       return <ErrorIndicator />;
     }
 
-    return <BookList books={books} />;
+    return <BookList books={books} onAddedToCart={onAddedToCart} />;
   }
 }
 
@@ -64,7 +68,8 @@ const mapStateToProps = ({books, loading, error}) => {
 
 const mapDispatchToProps = (dispatch, {bookstoreService}) => {
   return {
-    fetchBooks: fetchBooks(bookstoreService, dispatch)
+    fetchBooks: fetchBooks(bookstoreService, dispatch),
+    onAddedToCart: (id) => dispatch(bookAddedToCart(id))
   };
 };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,18 +3,18 @@ const initialState = {
   loading: true,
   error: null,
   cartItems: [
-    {
-      id: 1,
-      name: 'Book 1',
-      count: 3,
-      total: 150
-    },
-    {
-      id: 2,
-      name: 'Book 2',
-      count: 2,
-      total: 70
-    }
+    // {
+    //   id: 1,
+    //   name: 'Book 1',
+    //   count: 3,
+    //   total: 150
+    // },
+    // {
+    //   id: 2,
+    //   name: 'Book 2',
+    //   count: 2,
+    //   total: 70
+    // }
   ],
   orderTotal: 220
 };
@@ -46,6 +46,23 @@ const reducer = (state = initialState, action) => {
         books: [],
         loading: false,
         error: action.payload
+      };
+
+    case 'BOOK_ADDED_TO_CART':
+      const bookId = action.payload;
+      const book = state.books.find((book) => book.id === bookId);
+      const newItem = {
+        id: book.id,
+        name: book.title,
+        count: 1,
+        total: book.price
+      };
+      return {
+        ...state,
+        cartItems: [
+          ...state.cartItems,
+          newItem
+        ]
       };
 
     default:
